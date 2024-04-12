759f12a8ff4336fc02c8a3a9d308d689eacc03b3
Dilon Sok <sokdilon@gmail.com>
2024-04-12 13:28:38 -0500

    not finished with admin yet

--- 4301-voting/src/Admin.js
+++ 4301-voting/src/Admin.js
@@ -1,6 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import './Admin.css'; 
+
 function Admin() {
   return (
-   <div>hello</div>
+    <div className='admin-container'>
+      <div className='admin-text'>
+        hello i am dilon
+      </div>
+    </div>
   );
 }
 export default Admin;
