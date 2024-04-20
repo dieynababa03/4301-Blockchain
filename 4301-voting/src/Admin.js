fe2065eb6d5f099a26075a63261526da5f881e92
Dilon Sok <sokdilon@gmail.com>
2024-04-20 13:39:03 -0500

    cleaning up files, adding new directories
    will be finishing up admin today or tomorrow

--- 4301-voting/src/Admin.js
+++ 4301-voting/src/Admin.js
@@ -4,9 +4,22 @@ import './css/Admin.css';
 function Admin() {
   return (
     <div className='admin-container'>
-      <div className='admin-text'>
-        hello i am dilon
+      <div className='candidate-container'>
+        <input className='candidate-name' placeholder="Candidate Name"></input>
+        <div className='candidate-party'></div>
+        <div className='candidate-name'></div>
       </div>
+      <div className='date-container'>
+        <div className='start-date-container'>
+          <p>Start Date</p>
+          <input className='date-input' type='date'></input>
+        </div>
+        <div className='end-date-container'>
+          <p>End Date</p>
+          <input className='date-input' type='date'></input>
+        </div>
+      </div>
+      <button className='candidate-button'>Add Candidate</button>
     </div>
   );
 }
